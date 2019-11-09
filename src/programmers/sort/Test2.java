1586ee13b1c16e491944c043d9344dde1482bf2f
SpaceChae <enaenen@naver.com>
2019-11-09 14:40:26 +0900

    e

--- src/programmers/sort/Test2.java
+++ src/programmers/sort/Test2.java
@@ -1,4 +1,27 @@
 package programmers.sort;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Test2 {
+    public static void main(String[] args){
+        int[] t1 = {6,10,2};
+        int[] t2 = {3,30,34,5,9};
+    }
+    public static String solution(int[] numbers){
+        HashMap<Integer, List<Integer>> hm = new HashMap<Integer,List<Integer>>();
+        //List<Integer> calc = new ArrayList<Integer>();
+        int[] index = new int[numbers.length];
+        for(int i=0;i<numbers.length;i++){
+           // String calc = Integer.toString(numbers[i]).substring(0,1);
+
+            int numberLength=((int)Math.log10(numbers[i]));
+            index[i]=numbers[i] % ((int)Math.pow(10,numberLength));
+        }
+        Arrays.sort(numbers);
+
+
+        return "";
+    }
 }
