25cc79d88d4964e2a30cd6fb2c4c3115eace5d3b
space <enaenen@naver.com>
2019-11-17 19:09:47 +0900

    programmers sort - level 2 complete

--- src/programmers/sort/Test2.java
+++ src/programmers/sort/Test2.java
@@ -1,27 +1,33 @@
 package programmers.sort;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Test2 {
     public static void main(String[] args){
         int[] t1 = {6,10,2};
         int[] t2 = {3,30,34,5,9};
+//        System.out.println(331 / (int)Math.pow(10,(int)Math.log10(331)));
+        System.out.println(solution(t1));
     }
     public static String solution(int[] numbers){
-        HashMap<Integer, List<Integer>> hm = new HashMap<Integer,List<Integer>>();
-        //List<Integer> calc = new ArrayList<Integer>();
-        int[] index = new int[numbers.length];
-        for(int i=0;i<numbers.length;i++){
-           // String calc = Integer.toString(numbers[i]).substring(0,1);
-
-            int numberLength=((int)Math.log10(numbers[i]));
-            index[i]=numbers[i] % ((int)Math.pow(10,numberLength));
+        String[] list = new String[numbers.length];
+        for(int i=0;i<list.length;i++){
+            list[i]=Integer.toString(numbers[i]);
         }
-        Arrays.sort(numbers);
+        Arrays.sort(list,new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
 
+        if(list[0].equals("0"))
+            return "0";
 
-        return "";
+        String answer="";
+        for(String a : list){
+            answer+=a;
+        }
+        return answer;
     }
 }
