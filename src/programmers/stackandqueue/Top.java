b493efc85a610e14e8a61354751d1c72f906b8c1
space <enaenen@naver.com>
2019-11-26 05:25:34 +0900

    TOP problem solved

--- src/programmers/stackandqueue/Top.java
+++ src/programmers/stackandqueue/Top.java
@@ -19,31 +19,18 @@ public class Top {
         }
 
     }
-    public static int[] solution(int[] heights){
-        int[] answer =new int[heights.length];
-        Stack<Integer> stack = new Stack<Integer>();
-        for(int i : heights) {
-            stack.push(i);
-        }
-        List<Integer> list = new ArrayList<Integer>();
-        int stackCounter=1;
-        int standard = stack.pop();
-        while(true){
-            if(stack.empty())
-                break;
-            int compareNumber = stack.pop();
-            if(standard<compareNumber){
-                list.add(heights.length-stackCounter);
-                // standard=stack.pop();
-                standard=compareNumber;
-            }else{
-                list.add(0);
-                compareNumber=stack.pop();
-                stackCounter++;
+    public static int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            for(int j=i;j>=0;j--){
+                if(heights[i]<heights[j]){
+                    answer[i]=j+1;
+                    break;
+                }
+                else{
+                    answer[i]=0;
+                }
             }
-        }
-        for(int i=0;i<list.size();i++){
-            answer[i]=list.get(list.size()-1);
         }
         return answer;
     }
