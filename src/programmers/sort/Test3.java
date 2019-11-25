c21ecaf780efa91c692d2fc7288e35c8045bea06
space <enaenen@naver.com>
2019-11-25 21:54:40 +0900

    revert

---
+++ src/programmers/sort/Test3.java
@@ -0,0 +1,33 @@
+package programmers.sort;
+
+import java.util.Arrays;
+
+public class Test3 {
+    public static void main(String[] args){
+        int[] t1 = {350,452,877};
+        int[] t2 = {3,30,34,5,9};
+//        System.out.println(331 / (int)Math.pow(10,(int)Math.log10(331)));
+        System.out.println(solution(t1));
+    }
+    public static int solution(int[] citations){
+        int answer=0;
+        int tmp;
+        int remain;
+        int total = citations.length;
+
+        Arrays.sort(citations);
+        for(int i : citations) {
+            System.out.println(i);
+        }
+
+        for(int i=0;i<total;i++){
+            tmp = citations[i];
+            remain = total-i;
+            if(remain<=tmp){
+                answer = tmp;
+                break;
+            }
+        }
+        return answer;
+    }
+}
