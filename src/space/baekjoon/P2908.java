8decc926243537e996e05cc240dad91e760f6c49
space <enaenen@naver.com>
2020-04-04 05:57:30 +0900

    제목수정2098->2908

--- src/space/baekjoon/P2908.java
+++ src/space/baekjoon/P2908.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P2098 {
+public class P2908 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
