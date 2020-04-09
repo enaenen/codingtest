3d3b751345d59538e676bcb3c02402d95f5e100e
space <enaenen@naver.com>
2020-04-10 03:05:09 +0900

    fibo

--- src/space/baekjoon/recursive/P10870.java
+++ src/space/baekjoon/recursive/P10870.java
@@ -1,3 +1,25 @@
+/*
+피보나치 수열 출력(재귀)
+F(1)=1
+F(n)=F(n-1)+F(n-2)
+F(5) = 5
+
+F(5) = F(3)+F(4)
+     (1+1) + (1+ 1+1)
+
+F(4) = f(2)+f(3)
+        (1+)(1+1)
+
+F(3) = F(1)+f(2)
+        (1)+(1)
+
+F(2)=F(0)+F(1)
+      0 + 1
+
+F(1)<=1
+
+
+ */
 package space.baekjoon.recursive;
 
 import java.io.BufferedReader;
