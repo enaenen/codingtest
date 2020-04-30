b01761e5c9192a5f078418de9339d3d7f21b6976
space <enaenen@naver.com>
2020-04-30 21:50:27 +0900

    2447

---
+++ src/space/JavaCastingTest.java
@@ -0,0 +1,12 @@
+package space;
+
+public class JavaCastingTest {
+
+    protected final K TOMBSTONE = (K) (new Object());
+
+    public static void main(String[] args){
+    }
+}
+class K {
+
+}
--- src/space/baekjoon/recursive/P2447.java
+++ src/space/baekjoon/recursive/P2447.java
@@ -11,12 +11,32 @@ public class P2447 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        br.close();
+
         char[][] board = new char[n][n];
         star(board, n, 0,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
 
     }
     private static void star(char[][] board, int n, int x, int y){
-//
-//        star(board, )
+        if(n==1){
+            board[y][x]='*';
+            return;
+        }
+        star(board,n/3,x,y); //상좌
+        star(board,n/3,x+n/3,y);//상중
+        star(board, n/3,x+n/3*2,y);//상우
+
+        star(board,n/3,x,y+n/3);//중좌
+        star(board,n/3,x+n/3*2,y+n/3);//중우
+
+        star(board,n/3,x,y+n/3*2);//하좌
+        star(board, n/3,x+n/3,y+n/3*2);// 하중
+        star(board, n/3,x+n/3*2, y+n/3*2);//하우
     }
 }
