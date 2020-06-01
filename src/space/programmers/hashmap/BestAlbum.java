454a546552c4e4e2f13959110f94a2e895a3ec4e
space <enaenen@naver.com>
2020-06-01 13:37:11 +0900

    best album fix

--- src/space/programmers/hashmap/BestAlbum.java
+++ src/space/programmers/hashmap/BestAlbum.java
@@ -10,78 +10,49 @@ public class BestAlbum {
         String[] genres2 = {"a","b","c","d","e","a","a","b","b"};
         int[] plays2 =     {40,40,1,2,3,40,30,5,1};
 
-        int[] answer = new BestAlbum().solution(genres2,plays2);
+        String[] genres3 = {"a","b","c","d"};
+        int[] plays3 =     {1,1,1,1};
+
+        int[] answer = new BestAlbum().solution(genres3,plays3);
         for(int i=0;i<answer.length;i++){
             System.out.println(answer[i]);
         }
     }
     //HashMap에서 최고값을 찾기위한 메소드
-    private String findTopGenre(Map<String,Integer> map){
-        Map.Entry<String,Integer> maxEntry = null;
-        for(Map.Entry<String,Integer> entry : map.entrySet()){
-            if(maxEntry==null || entry.getValue().compareTo(maxEntry.getValue())>0){
-                maxEntry = entry;
-            }
-        }
-        return maxEntry.getKey();
-    }
+
     public int[] solution(String[] genres, int[] plays) {
         if(plays.length==1){
             return new int[]{0};
         }
 
-        ArrayList<Music> list = new ArrayList<Music>();
+        ArrayList<Music> musicList = new ArrayList<Music>();
         HashMap<String,Integer> genreCounter = new HashMap<String,Integer>();
 
         for(int i=0;i<genres.length;i++){
-            list.add(new Music(genres[i],plays[i],i));
+            musicList.add(new Music(genres[i],plays[i],i));
             genreCounter.put(genres[i],genreCounter.getOrDefault(genres[i],0)+plays[i]);
         }
         //재생수 별 장르 2개 고르기
-        String topGenre=findTopGenre(genreCounter);
-        genreCounter.remove(topGenre);
-        String secondGenre=findTopGenre(genreCounter);
+        ArrayList<String> topGenreList = new ArrayList<String>(genreCounter.keySet());
+        Collections.sort(topGenreList,(o1,o2) -> (genreCounter.get(o2).compareTo(genreCounter.get(o1))));
 
-
-        for(int i=0;i<list.size();i++){
-            System.out.println("plays : "+             list.get(i).getPlays()+" "
-                    +"NO : "+ list.get(i).getNo()+" "+
-                    "Genre : "+list.get(i).getGenre());
-
-        }
-        System.out.println();
-        //재생 수 높은 2곡 찾기
-        Collections.sort(list);
-        for(int i=0;i<list.size();i++){
-            System.out.println("plays : "+             list.get(i).getPlays()+" "
-            +"NO : "+ list.get(i).getNo()+" "+
-                    "Genre : "+list.get(i).getGenre());
-
-        }
-
+        //재생 수 높은 곡 순으로 정렬
+        Collections.sort(musicList);
 
         //answer List 작성
         ArrayList<Integer> answerList = new ArrayList<Integer>();
-        int cnt =0;
-        for(int i=0;i<list.size();i++){
 
-        }
-        for(int j=0;j<list.size();j++){
-            String g = list.get(j).getGenre();
-            int number = list.get(j).getNo();
-            if(g.compareTo(topGenre)==0 && cnt<2) {
-                answerList.add(number);
-               // list.remove(j);
-                cnt++;
-            }
-        }
-        cnt=0;
-        for(int j=0;j<list.size();j++){
-            String g = list.get(j).getGenre();
-            int number = list.get(j).getNo();
-            if(g.compareTo(secondGenre)==0 && cnt<2) {
-                answerList.add(number);
-                cnt++;
+        for(int i=0;i<topGenreList.size();i++){
+            int cnt =0;
+            String toFindGenre = topGenreList.get(i);
+            for(int j=0;j<musicList.size();j++){
+                if(musicList.get(j).getGenre().compareTo(toFindGenre)==0){
+                    cnt++;
+                    if(cnt==2){
+                        break;
+                    }
+                    answerList.add(musicList.get(j).getNo());
+                }
             }
         }
 
@@ -89,9 +60,7 @@ public class BestAlbum {
         for(int i=0;i<answerList.size();i++){
             answer[i]=answerList.get(i);
         }
-//        for(int i=0;i<list.size();i++){
-//            System.out.println(list.get(i).getGenre() +" " + list.get(i).getNo());
-//        }
+
         return answer;
     }
     private class Music implements Comparable<Music>{
