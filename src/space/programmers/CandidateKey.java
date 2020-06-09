07ea4821425fb2b9555779f7c5c225f24419739b
SpaceChae <enaenen@naver.com>
2020-06-09 23:34:05 +0900

    0609

---
+++ src/space/programmers/CandidateKey.java
@@ -0,0 +1,25 @@
+package space.programmers;
+
+public class CandidateKey {
+    public static void main(String[] args){
+        String[][] a = new String[][] {
+                {"100","ryan","music","2"},
+                {"200","apeach","math","2"},
+                {"300","tube","computer","3"},
+                {"400","con","computer","4"},
+                {"500","muzi","music","3"},
+                {"600","apeach","music","2"}
+        };
+    }
+
+    public int solution(String[][] relation) {
+        int answer = 0;
+        StringBuilder cache = new StringBuilder();
+        String[] tuple = new String[relation.length];
+
+
+
+
+        return answer;
+    }
+}
