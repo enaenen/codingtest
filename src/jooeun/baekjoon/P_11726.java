ee7d10351bc735482ca6195ee6225de8386b4417
ChungJooEun <nina2439@naver.com>
2021-03-18 03:09:00 +0900

    백준 11726번 문제 Dynamic programming 문제

--- src/jooeun/baekjoon/P_11726.java
+++ src/jooeun/baekjoon/P_11726.java
@@ -12,32 +12,35 @@ public class P_11726 {
     }
 
     //top -> down : 하향식
-    public static int solution(int n){
-
-        int[] dp = new int[n + 3];
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for(int i = 2; i <= n; i++)
-
-            dp[i] = dp[i + 1] + dp[i + 2];
-
-        return dp[n];
-    }
-
-    // down -> top : 상향식
-//    public static int solution(int n){
+//    public static int solution(int n) {
 //
-//        int[] dp = new int[n+3];
+//        int[] dp = new int[n + 3];
+//        dp[0] = 1;
+//        dp[1] = 1;
 //
-//        for(int i = 0 ; i <= n ; i++){
-//
-//            dp[i + 1] += dp[i];
-//            dp[i + 2] += dp[i];
-//
+//        for (int i = 2; i <= n; i++){
+//            dp[i] = dp[i - 1] + dp[i - 2];
 //        }
 //
 //        return dp[n];
 //    }
+
+    // down -> top : 상향식
+    public static int solution(int n){
+
+        int[] dp = new int[n+3];
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 0 ; i <= n ; i++){
+
+            dp[i + 1] += dp[i];
+            dp[i + 2] += dp[i];
+
+            System.out.println((i+1) +" "+(i+2)+"/ "+dp[i+1]+" "+dp[i+2]);
+        }
+
+        return dp[n];
+    }
 
 }
