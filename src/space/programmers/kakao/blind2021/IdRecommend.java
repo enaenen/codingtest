413443d4642ec71ef274a0b868c08b3fa6f29c04
space <enaenen@naver.com>
2021-03-23 19:19:07 +0900

    https://programmers.co.kr/learn/courses/30/lessons/72410 - 신규아이디추천

--- src/space/programmers/kakao/blind2021/IdRecommend.java
+++ src/space/programmers/kakao/blind2021/IdRecommend.java
@@ -1,56 +1,27 @@
 package space.programmers.kakao.blind2021;
 
+
 public class IdRecommend {
     public static void main(String[] args) {
         System.out.println(solution("=.="));
     }
     public static String solution(String new_id) {
-        StringBuilder as = new StringBuilder();
         String answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^-_.a-z0-9]","");
+        answer = answer.replaceAll("[.]{2,}",".");
+        answer = answer.replaceAll("^[.]|[.]$","");
 
-        char[] chars = answer.toCharArray();
-        int length = chars.length;
-        int i = 0;
-        if(chars[0]=='.')
-            chars[0]=' ';
-        if(chars[chars.length-1]=='.')
-            chars[length-1]=' ';
-
-        while(i<length){
-            if(('a'<=chars[i] && chars[i] <='z')||('A'<=chars[i] && chars[i]<='Z')||chars[i]=='-'||chars[i]=='_'||chars[i]=='.')
-                as.append(chars[i]);
-            i++;
+        if(answer.length()>=16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$","");
         }
-        answer = as.toString();
-        while(answer.contains("..")) {
-            answer = answer.replace("..", ".");
-        }
-        if(answer.length()>0){
-            if(answer.charAt(0)=='.'){
-                answer=answer.substring(1,answer.length()-1);
+        if(answer=="" || answer.length()==0)
+            answer+="a";
+        if(answer.length() <= 2){
+            while(answer.length() < 3) {
+                answer += answer.charAt(answer.length() - 1);
             }
         }
-        if(answer.length()>0){
-            if(answer.charAt(answer.length()-1)=='.'){
-                answer=answer.substring(0,answer.length()-1);
-            }
-        }
-        if(answer.equals(""))
-            answer="a";
-
-        if(answer.length()>=16){
-            answer=answer.substring(0,15);
-            if(answer.charAt(answer.length()-1)=='.')
-                answer=answer.substring(0, answer.length()-1);
-        }
-
-        StringBuilder sbLast = new StringBuilder(answer);
-        if(sbLast.length()<=2) {
-            char last = sbLast.charAt(sbLast.length() - 1);
-            while(sbLast.length() < 3)
-                sbLast.append(last);
-        }
-        answer = sbLast.toString();
         return answer;
     }
 }
