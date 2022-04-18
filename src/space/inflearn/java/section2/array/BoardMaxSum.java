9c1b1e4af47590f3e823ddc71fd2b5db981cd22e
Space <enaenen@naver.com>
2022-04-19 02:04:00 +0900

    격자판 최대합

---
+++ src/space/inflearn/java/section2/array/BoardMaxSum.java
@@ -0,0 +1,76 @@
+package space.inflearn.java.section2.array;
+
+import java.util.Scanner;
+
+/**
+ * https://cote.inflearn.com/contest/10/problem/02-09
+ *
+ * 9. 격자판 최대합
+ *설명
+ *
+ * 5*5 격자판에 아래롸 같이 숫자가 적혀있습니다.
+ * N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가 장 큰 합을 출력합니다.
+ *
+ *
+ * 입력
+ * 첫 줄에 자연수 N이 주어진다.(2<=N<=50)
+ *
+ * 두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다.
+ *
+ *
+ * 출력
+ * 최대합을 출력합니다.
+ *
+ * 5
+ * 10 13 10 12 15
+ * 12 39 30 23 11
+ * 11 25 50 53 15
+ * 19 27 29 37 27
+ * 19 13 30 13 19
+ *
+ * 155
+ *
+ */
+public class BoardMaxSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        int max = 0;
+        int sum;
+        //가로 확인
+        for (int i = 0; i < n; i++)
+        {
+            sum = 0;
+            for (int j = 0; j < n ; j++)
+                sum += board[i][j];
+            if (max < sum)
+                max = sum;
+        }
+        //세로 확인
+        for (int i = 0; i < n; i++)
+        {
+            sum = 0;
+            for (int j = 0; j < n ; j++)
+                sum += board[j][i];
+            if (max < sum)
+                max = sum;
+        }
+        //대각선 확인
+        sum = 0;
+        for (int i = 0; i < n; i++)
+           sum += board[i][i];
+        if (max < sum)
+            max = sum;
+
+        System.out.println(max);
+    }
+}
