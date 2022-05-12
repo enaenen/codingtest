b5cabdcea0e9b2b399c9f1c63e9138e94438f3cd
Woo Joo Chae <enaenen@naver.com>
2022-05-12 23:15:40 +0900

    array_test

---
+++ src/space/leetcode/DesignAddAndSearchWords.java
@@ -0,0 +1,48 @@
+//package space.leetcode;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//class WordDictionary {
+//    private List<String> words;
+//
+//    public WordDictionary() {
+//        words = new ArrayList<String>();
+//    }
+//    Map<Charactermz>
+//
+//    public void addWord(String word) {
+//        words.add(word);
+//    }
+//
+//    public boolean search(String word) {
+//        if (word.contains(".")){
+//            for (String findWord : words){
+//                findWord.matches()
+//            }
+//        }
+//        else{
+//            for (String findWord : words){
+//                if (word.equals(findWord))
+//                    return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
+//
+///**
+// * Your WordDictionary object will be instantiated and called as such:
+// * WordDictionary obj = new WordDictionary();
+// * obj.addWord(word);
+// * boolean param_2 = obj.search(word);
+// */
+//public class DesignAddAndSearchWords {
+//    public static void main(String[] args) {
+//        String word = "ab";
+//        String searchWord = "..";
+//        System.out.println(word.matches(searchWord));
+//    }
+//
+//
+//}
