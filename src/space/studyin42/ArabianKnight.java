df5be4e5ca1810031f857a9f08d22bc0c34d37a2
Woo Joo Chae <enaenen@naver.com>
2022-06-23 20:07:51 +0900

    나이트 이동

---
+++ src/space/studyin42/ArabianKnight.java
@@ -0,0 +1,27 @@
+package space.studyin42;
+
+import java.util.Scanner;
+
+public class ArabianKnight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] inputs = sc.next().toCharArray();
+        int[][] set = {{-2, 1}, {-2, -1}, {2, 1}, {2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}};
+        int x, y;
+        int cnt = 0;
+        
+        x = inputs[0] - 'a' + 1;
+        y = Integer.parseInt(String.valueOf(inputs[1]));
+        
+        for (int[] ints : set) {
+            int tmpX = x + ints[0];
+            int tmpY = y + ints[1];
+            if (1 <= tmpX && tmpX <= 8 && 1 <= tmpY && tmpY <= 8) {
+                cnt++;
+//                System.out.println("ints[0] = " +ints[0] + " ints[1]= " +ints[1]);
+//                System.out.println("tmpX = "+tmpX + " tmpY = " + tmpY);
+            }
+        }
+        System.out.println(cnt);
+    }
+}
