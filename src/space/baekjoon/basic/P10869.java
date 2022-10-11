fba57ff975f975a24464b7b997ac701650075265
Space <enaenen@naver.com>
2022-10-12 04:04:43 +0900

    boj study

---
+++ src/space/baekjoon/basic/P10869.java
@@ -0,0 +1,19 @@
+package space.baekjoon.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
