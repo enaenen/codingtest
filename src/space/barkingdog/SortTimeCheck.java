8265009f2879d1d3a7455e8831e92915b2849471
SpaceChae <enaenen@naver.com>
2022-10-13 00:02:31 +0900

    tests

--- src/space/barkingdog/SortTimeCheck.java
+++ src/space/barkingdog/SortTimeCheck.java
@@ -1,5 +1,8 @@
 package space.barkingdog;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 
@@ -18,6 +21,7 @@ public class SortTimeCheck {
         Arrays.sort(arr);
         long afterTime = System.nanoTime();
         long diffSec = afterTime - beforeTime;
+        int[] ints = Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
         System.out.println("diff = " + diffSec);
 
     }
@@ -26,16 +30,33 @@ public class SortTimeCheck {
         int max = 100;
         return (int) ((Math.random() * (max - min)) + min);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int[] arr = new int[3];
-        String[] strings = new String[3];
-        for (int i = 0; i <3; i++){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int[] ints = Arrays.stream(inputs).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(ints);
+        for (Integer a : ints){
+            System.out.print(a + " ");
+        }
+/*
+        int[] arr = new int[1000000]; //= new int[] {3, 2,1};
+        String[] strings = new String[1000000]; //= new String[] {"3","2","1"};
+        for (int i = 0; i <1000000; i++){
             arr[i] = randomIntGenerator();
             strings[i] = String.valueOf(arr[i]);
         }
+
         sortingString(strings);
         sortingInteger(arr);
+//        for (String string : strings) {
+//            System.out.print(string + " ");
+//        }
+//        for (int i : arr) {
+//            System.out.print(i + " ");
+//        }
+*/
+ }
 
-    }
+
 }
