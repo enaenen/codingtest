34676772c6c1e16c5e3fe22666b3ce74cbfedd64
SpaceChae <enaenen@naver.com>
2022-10-25 12:57:49 +0900

    test

--- src/space/barkingdog/firstWeek/CompareToTest.java
+++ src/space/barkingdog/firstWeek/CompareToTest.java
@@ -1,6 +1,9 @@
 package space.barkingdog.firstWeek;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class CompareToTest {
     public static void main(String[] args) {
@@ -9,6 +12,8 @@ public class CompareToTest {
         //100 99 98 97
         //97 99
         int[] a = {3, 2, 1};
+//        HashSet<Integer> hs = new HashSet<>(a.to);
+        System.out.println(Arrays.stream(a).max().getAsInt());
         Arrays.sort(a);
         System.out.println("ab = " + ab);
     }
