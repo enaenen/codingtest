98282c98cd6f4c8da56111686be081637b281f4e
Space <enaenen@naver.com>
2022-11-14 09:10:24 +0900

    PalindromeNumber

--- src/space/leetcode/PalindromeNumber.java
+++ src/space/leetcode/PalindromeNumber.java
@@ -1,6 +1,10 @@
 package space.leetcode;
 
 public class PalindromeNumber {
+    public static final String CAT = "\\    /\\\n" +
+            " )  ( ')\n" +
+            "(  /  )\n" +
+            " \\(__)|";
     public boolean isPalindrome(int x) {
         String numberString = Integer.toString(x);
         return Integer.toString(x).equals(new StringBuilder(numberString).reverse().toString());
