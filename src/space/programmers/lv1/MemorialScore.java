b1a7dd4d30e1dfb1d5d092d52f1c09493174a823
Space <enaenen@naver.com>
2023-06-11 23:17:58 +0900

    Lv1 추억점수

--- src/space/programmers/lv1/MemorialScore.java
+++ src/space/programmers/lv1/MemorialScore.java
@@ -18,7 +18,7 @@ public class MemorialScore {
     public static int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
         for (int i = 0; i < photo.length; ++i) {
-            for (int j = 0; j < photo.length; ++j) {
+            for (int j = 0; j < photo[i].length; ++j) {
                 if (Arrays.asList(name).indexOf(photo[i][j]) != -1)
                     answer[i] += yearning[Arrays.asList(name).indexOf(photo[i][j])];
             }
