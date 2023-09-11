8dbaf4df5786b88ee44b4fa2d814b9d141136e19
space <enaenen@naver.com>
2023-09-12 07:41:01 +0900

    https://leetcode.com/problems/max-consecutive-ones-iii/submissions/?envType=study-plan-v2&envId=leetcode-75

--- src/space/leetcode/leetcode75/medium/MaxConsecutiveOnes3.java
+++ src/space/leetcode/leetcode75/medium/MaxConsecutiveOnes3.java
@@ -1,40 +1,38 @@
 package space.leetcode.leetcode75.medium;
-
+// 구간 구하기 문제
 public class MaxConsecutiveOnes3 {
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
         int[] nums2 = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+        int[] nums3 = {0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0};
 
         int k = 2;
 
         int result = longestOnes(nums, k);
         int result2 = longestOnes(nums2, k);
+        int result3 = longestOnes(nums3, k);
 
         System.out.println(result);
         System.out.println(result2);
+        System.out.println(result3);
     }
 
     public static int longestOnes(int[] nums, int k) {
-        int maxRange = Integer.MIN_VALUE;
-        int start = 0, end = 0;
+        int start = 0, end = 0, zero = 0;
 
         while (end < nums.length) {
-            int acceptableZero = k;
-
             if (nums[end] == 0)
-                acceptableZero--;
-            end++;
-            if (acceptableZero < 0){
+                zero++;
 
+            end++;
+
+            if (zero > k) {
+                if (nums[start] == 0)
+                    zero--;
+
+                start++;
             }
-            else {
-                while (acceptableZero > 0) {
-                    --acceptableZero;
-                    ++end;
-                }
-            }
-            maxRange = Math.max(maxRange, (end - start));
         }
-        return maxRange;
+        return end - start;
     }
 }
